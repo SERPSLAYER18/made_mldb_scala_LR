d7df812bda82699d0bd267e939fde9e2ba8360c7
Kirill Lyahnovich <kirill.lyahnovich@gmail.com>
2022-11-24 18:29:24 +0300

    MLBD LAB 3

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -10,17 +10,25 @@ object Main extends App {
   val TRAIN_FILENAME = "train_data.csv"
   val TEST_FILENAME = "test_data.csv"
   val OUTPUT_FILENAME = "predictions.csv"
+
+
+  var train = csvread(new File(TRAIN_FILENAME), ',')
   val target = train(::, (train.cols - 1))
-  val result = leastSquares(train, target)
   train = train.delete(train.cols - 1, Axis._1)
+
+  var test = csvread(new File(TEST_FILENAME), ',')
   val y_true = test(::, (test.cols - 1))
+  test = test.delete(test.cols - 1, Axis._1)
+
+  val result = leastSquares(train, target)
+
+
   val y_pred = result.apply(test)
   val mape = sum(abs(y_pred - y_true) / y_true) / test.rows
-  test = test.delete(test.cols - 1, Axis._1)
+
   val concated = DenseMatrix.horzcat(test, new DenseMatrix(test.rows, 1, y_pred.toArray))
-  var train = csvread(new File(TRAIN_FILENAME), ',')
   println("MAE: " + mape * 100 + "%")
-  var test = csvread(new File(TEST_FILENAME), ',')
-  println(concated)
+
   csvwrite(new File(OUTPUT_FILENAME), concated, separator = ',')
+  println("Predictions are saved into " + OUTPUT_FILENAME)
 }
